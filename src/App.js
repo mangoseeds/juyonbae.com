116484f2bd8ffb2f91c908c7b6762b702b16b330
J Bae <juyonbae@gmail.com>
2023-04-09 16:21:02 -0400

    cursor style added, landing page completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 import './App.css';
+import { useNavigate } from "react-router-dom";
+
+import Contact from './Contact';
 
 function App() {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0
+  });
+  // console.log(mousePosition);
+
+  const [cursorVariant, setCursorVariant] = useState("default");
+
+
+  useEffect(() => {
+    const mouseMove = e => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY
+      })
+    }
+
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove)
+    }
+  }, []);
+
+
+  const variants = {
+    default: {
+      x: mousePosition.x - 10,
+      y: mousePosition.y - 10
+    },
+    button: {
+      x: mousePosition.x - 35,
+      y: mousePosition.y - 35,
+      height: 70,
+      width: 70,
+      backgroundColor: 'rgba(40,40,40,0.5)',
+      mixBlendMode: "diff"
+    }
+  }
+
+  const buttonEnter = () => setCursorVariant("button");
+  const buttonLeave = () => setCursorVariant("default");
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hi there
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <motion.main
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.5, duration: 2 }}
+          >
+            <div className="top">
+                <h1>Juyon Bae</h1>
+            </div>
+            <div className="main-page-item-container">
+                    <button onMouseEnter={buttonEnter} onMouseLeave={buttonLeave} className="main-page-item">about</button>
+                    <button onMouseEnter={buttonEnter} onMouseLeave={buttonLeave} className="main-page-item">projects</button>
+                    <button onMouseEnter={buttonEnter} onMouseLeave={buttonLeave} className="main-page-item">experience</button>
+                    <button onMouseEnter={buttonEnter} onMouseLeave={buttonLeave} className="main-page-item">contact</button>
+            </div>
+        </motion.main>
+        <motion.div 
+          className="cursor"
+          variants={variants}
+          animate={cursorVariant}
+        />
+      </body>
     </div>
+
   );
 }
 
